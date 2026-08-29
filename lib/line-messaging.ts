cbcbf13499e37bb18b9a283eb705d6b40cb747dc
yano <yano.2001.shop@gmail.com>
2026-08-29 20:10:24 +0900

    fix mitate LINE notification

--- lib/line-messaging.ts
+++ lib/line-messaging.ts
@@ -33,7 +33,7 @@ export function createMitateFlexContents(mitateUrl: string) {
         {
           type: "button",
           style: "primary",
-          height: "xl",
+          height: "md",
           weight: "bold",
           color: "#49B8B1",
           action: {
